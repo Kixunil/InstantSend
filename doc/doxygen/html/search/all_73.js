6ae49cd7773ba56029bd2929db9a6921d2f45a5c
Martin Habovštiak <martin.habovstiak@gmail.com>
2013-01-04 14:50:48 +0100

    Updated version number in documentation

--- doc/doxygen/html/search/all_73.js
+++ doc/doxygen/html/search/all_73.js
@@ -4,6 +4,7 @@ var searchData=
   ['seconnect',['seconnect',['../classsecurityCreator__t.html#a034aee313d01680b4cd7538a427ffdfe',1,'securityCreator_t']]],
   ['securitycreator_5ft',['securityCreator_t',['../classsecurityCreator__t.html',1,'']]],
   ['senddata',['sendData',['../classpeer__t.html#a42902fbacfebd9773f4f7ad3527382be',1,'peer_t']]],
+  ['servercontroller_5ft',['serverController_t',['../classserverController__t.html',1,'']]],
   ['serverfilelist_5ft',['serverFileList_t',['../classserverFileList__t.html',1,'']]],
   ['serverplugin_5ft',['serverPlugin_t',['../classserverPlugin__t.html',1,'']]],
   ['serverthread_5ft',['serverThread_t',['../classserverThread__t.html',1,'']]],
