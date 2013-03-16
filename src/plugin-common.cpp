264cf705d2ef3fea449ea300e1b1f1b7ac9b1917
Martin Habovštiak <martin.habovstiak@gmail.com>
2013-03-16 09:42:06 +0100

    Common plugin API interface changed according to other changes

--- src/plugin-common.cpp
+++ src/plugin-common.cpp
@@ -7,6 +7,17 @@ pluginInstance_t::~pluginInstance_t() {
 pluginInstanceCreator_t::~pluginInstanceCreator_t() {
 }
 
+bool pluginInstanceCreator_t::unloadPossible() {
+	return true;
+}
+
+bool pluginMultiInstanceCreator_t::unloadPossible() {
+	return !count;
+}
+
+pluginMultiInstanceCreator_t::~pluginMultiInstanceCreator_t() {
+}
+
 #if 0
 event_t::~event_t() {
 }
