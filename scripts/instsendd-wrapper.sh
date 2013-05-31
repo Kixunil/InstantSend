7714dd100fe70d07f8f462b5a3550b2a638fb14a
Martin Habovštiak <martin.habovstiak@gmail.com>
2013-05-31 11:57:28 +0200

    instsendd-wrapper now uses fifolocker

--- scripts/instsendd-wrapper.sh
+++ scripts/instsendd-wrapper.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
+LOCK_DIR="$HOME/.instantsend/lock_server"
 LOCK_PARENT="$HOME/.instantsend/"
 LOCK_NAME="lock"
 PID_FILE="$HOME/.instantsend/pid"
 
 is_start() {
-	mkdir -p "$LOCK_PARENT" # Make sure parent exists
-	if mkdir "$HOME/.instantsend/lock" &> /dev/null; # Obtain lock
-	then
-		instsendd &> /dev/null & # Run instantsend
-		echo $! > "$PID_FILE" # Store PID
-		return 0
-	else
-		echo "Can't obtain lock - InstantSend is probably already running"
-		return 1
-	fi
+	mkdir -p "$LOCK_DIR" # Make sure it exists
+	instsend-fifolocker "$LOCK_DIR" instsendd --daemon --pid-file="$PID_FILE" # Run instantsend
+	return $?
 }
 
 is_stop() {
-	if ISPID=`cat "$PID_FILE" 2>/dev/null`;
+	instsend-fifolocker "$LOCK_DIR" true
+	RET=$?
+	if [ $RET -eq 47 ];
 	then
-		kill "$ISPID" && rm -rf  "$PID_FILE" "$LOCK_PARENT/$LOCK_NAME"
+		if ISPID=`cat "$PID_FILE" 2>/dev/null`;
+		then
+			kill -SIGINT "$ISPID"
+			return 0
+		else
+			echo "Can't get PID of InstantSend - probably it's not running"
+			return 1
+		fi
 	else
-		echo "Can't get PID of InstantSend - probably it's not running"
+		echo "InstantSend is not running"
+		return 1
 	fi
 }
 
@@ -44,7 +48,9 @@ case $1 in
 		is_start && exit 0 || exit 1
 		;;
 	check)
-		if PID=$(cat "$PID_FILE" &>/dev/null) && ps ax | grep -q '^ '"$PID";
+		instsend-fifolocker "$LOCK_DIR" true
+		RET=$?
+		if [ $RET -eq 47 ];
 		then
 			exit 0
 		else
