3d86bcc826294ead888f7bd1a4a4e91f98a7af5f
Martin Habovštiak <martin.habovstiak@gmail.com>
2013-05-31 11:55:08 +0200

    Updated dependencies

--- install-debian-deps.sh
+++ install-debian-deps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-apt-get install libdbus-1-dev libnotify-dev libgtkmm-2.4-dev libavahi-common-dev libavahi-client-dev
+apt-get install libdbus-1-dev libnotify-dev libgtkmm-2.4-dev libavahi-common-dev libavahi-client-dev avahi-utils libssl-dev doxygen zenity
