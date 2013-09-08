785833dd8efbe7394abdeafd75d44ce1f75e3aaa
Martin Habovštiak <martin.habovstiak@gmail.com>
2013-09-08 22:21:02 +0200

    Revision number increased; README updated a little

--- buildpackage.sh
+++ buildpackage.sh
@@ -13,7 +13,7 @@ fi
 BINARY=""
 
 INSTANTSEND_VERSION=`grep '^AC_INIT' configure.ac | sed -re 's/^.*\[.*\].*\[([0-9.]*)\].*\[.*\].*$/\1/'`
-REVISION=2
+REVISION=3
 
 # this function was found at http://stackoverflow.com/questions/4023830/bash-how-compare-two-strings-in-version-format
 vercomp () {
