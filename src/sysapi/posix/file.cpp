99997c65d55814a765dadebb1e7ea7f94d14fd5f
Martin Habovštiak <martin.habovstiak@gmail.com>
2013-10-24 16:01:51 +0200

    Improved error reporting

--- src/sysapi/posix/file.cpp
+++ src/sysapi/posix/file.cpp
@@ -18,11 +18,15 @@
 	#define lseek64 lseek
 #endif
 
+#define MKSTR(NUM) #NUM
+#define TOSTRING(ARG) MKSTR(ARG)
+#define POS (__FILE__ ":" TOSTRING(__LINE__))
+
 using namespace std;
 
 class EAutoFile: public EFileError {
 	public:
-		EAutoFile() : EFileError(strerror(errno)) {}
+		EAutoFile(const string &prefix) : EFileError(prefix + ": " + strerror(errno)) {}
 		~EAutoFile() throw() {}
 };
 
@@ -31,31 +35,31 @@ class PosixFile : public File::Data {
 		inline PosixFile(int fd) : mFd(fd) {}
 		PosixFile(const string &filename, bool writeable) : mFd(writeable?open(filename.c_str(), O_RDWR | O_LARGEFILE | O_CREAT, S_IRUSR | S_IWUSR):open(filename.c_str(), O_RDONLY | O_LARGEFILE)) {
 			//fprintf(stderr, "Opening %s\n", filename.c_str());
-			if(mFd < 0) throw EAutoFile();
+			if(mFd < 0) throw EAutoFile("open");
 		}
 
 		File::Size readData(char *data, File::Size size, File::Size position) {
-			if(lseek64(mFd, position, SEEK_SET) == -1) throw EAutoFile();
+			if(lseek64(mFd, position, SEEK_SET) == -1) throw EAutoFile(string(POS) + ": lseek64");
 			File::Size result = read(mFd, data, size);
-			if(result == -1) throw EAutoFile();
+			if(result == -1) throw EAutoFile(string(POS) + ": read");
 			return result;
 		}
 
 		File::Size readData(char *data, File::Size size) {
 			File::Size result = read(mFd, data, size);
-			if(result == -1) throw EAutoFile();
+			if(result == -1) throw EAutoFile(string(POS) + ": read");
 			return result;
 		}
 
 		void writeData(const char *data, File::Size size, File::Size position) {
-			if(lseek64(mFd, position, SEEK_SET) == -1) throw EAutoFile();
+			if(lseek64(mFd, position, SEEK_SET) == -1) throw EAutoFile(string(POS) + ": lseek64");
 			while(size) {
 				ssize_t len = write(mFd, data, size);
 				if(len < 0) switch(errno) {
 					case ENOSPC:
 						throw ENoSpace();
 					default:
-						throw EAutoFile();
+						throw EAutoFile(string(POS) + ": write");
 				}
 				size -= len;
 				data += len;
@@ -65,12 +69,12 @@ class PosixFile : public File::Data {
 		File::Size getSize() {
 			// Remember current position
 			File::Size oldpos = lseek64(mFd, 0, SEEK_CUR);
-			if(oldpos == -1) throw EAutoFile();
+			if(oldpos == -1) throw EAutoFile(string(POS) + ": lseek64");
 			// Seek to end, to find out file size
 			File::Size result = lseek64(mFd, 0, SEEK_END);
-			if(result == -1) throw EAutoFile();
+			if(result == -1) throw EAutoFile(string(POS) + ": lseek64");
 			// Return to previous position
-			if(lseek64(mFd, oldpos, SEEK_SET) == -1) throw EAutoFile();
+			if(lseek64(mFd, oldpos, SEEK_SET) == -1) throw EAutoFile(string(POS) + ": lseek64");
 
 			return result;
 		}
